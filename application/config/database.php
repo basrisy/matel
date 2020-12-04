8bad5b69d2f67d6592af4899a07615ef0b5bc591
Syaiful Basri <50867488+basrisy@users.noreply.github.com>
2020-12-04 10:42:42 +0700

    upload_hosting4122020

--- application/config/database.php
+++ application/config/database.php
@@ -75,6 +75,28 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'onek5663_onematel',
+	'password' => 'Tujuh0511',
+	'database' => 'onek5663_onematel',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db[''] = array(
+	'dsn'	=> '',
 	'hostname' => '36.91.140.211:3377',
 	'username' => 'root',
 	'password' => '##Urban2018##',
